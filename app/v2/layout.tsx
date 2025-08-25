fa6324b75c210410250536a234ae0bf13fe065c8
a3eu <azgh@nenaro.com>
2025-08-24 22:14:51 -0700

    typo

--- app/v2/layout.tsx
+++ app/v2/layout.tsx
@@ -11,9 +11,8 @@ const kaushanScript = Kaushan_Script({
 })
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Con Música En Vivo website',
+  description: 'Con Música En Vivo website',
 }
 
 export default function RootLayout({
